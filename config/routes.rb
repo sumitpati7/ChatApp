8d6553c677c7d94503ed3e828267c38c2c330c7e
sumitpati7 <sumitpati7@gmail.com>
2024-12-23 12:50:24 +0545

    devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations",
+    unlocks: "users/unlocks",
+    passwords: "users/passwords",
+    omniauth: "users/omniauth",
+    confirmations: "users/confirmations"
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
